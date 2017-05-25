a318d7cda54ce30b210984cced7491544e93b59b
Juan Garcia <juangarcia@Juans-Air.home>
2017-05-25 17:59:07 +0200

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import CountDownComponent from './components/countDown.js';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class CountDown extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CountDownComponent time={2222} currentTimeToBegin={2225}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('CountDown', () => CountDown);
